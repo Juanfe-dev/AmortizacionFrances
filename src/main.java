e883e37faa5cf84b06ccdbd5b1e8800c9b241173
JuanFe <juan.gomez164@udea.edu.co>
2022-06-28 11:00:33 -0500

    Segundo Commit

--- src/main.java
+++ src/main.java
@@ -1,13 +1,20 @@
+import java.io.IOException;
+import java.util.InputMismatchException;
+
 
 public class main {
 
-	
 	public static void main(String[] args) {
-			Run();
+		Run();
 	}
-	
+
 	public static void Run() {
 		FrenchAmortization credit = new FrenchAmortization();
-		credit.userDataInput();
+		try {
+			credit.userDataInput();
+		} catch (InputMismatchException ex) {
+			System.out.println("Intente de nuevo\nPor favor ingrese un valor numerico sin signos de puntuacion. ");
+			Run();
+		}
 	}
 }
